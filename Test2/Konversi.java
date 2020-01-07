0decd19327e60b535eb2bd45f2929fc54d89525d
Dian Ratri Hapsari <dianratrihapsari@gmail.com>
2020-01-07 17:00:22 +0700

    test konversi

---
+++ Test2/Konversi.java
@@ -0,0 +1,11 @@
+public class Konversi{
+    public static void main(String[] args){
+        int pon = 2;
+        int kovers;
+
+        for (int i=0;i<101;i++){
+            total = pon * i;
+            System.out.println(i+" kg = "+total+" pon");
+    }
+
+}
